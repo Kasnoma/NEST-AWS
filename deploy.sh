3f9ac737115ad8bbf3eb45548be04d63bd87323d
Kasnoma <91282375+Kasnoma@users.noreply.github.com>
2025-01-15 15:10:39 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+echo "[>] Starting deployment to server"
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+sudo apt-get update
+
+sudo ./aws/install --update
+
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+
+aws configure set default.region $AWS_DEFAULT_REGION
+
+aws --version
+
+aws ssm send-command --document-name "AWS-RunShellScript" --document-version "1" --targets '[{"Key":"InstanceIds","Values":["i-08482125e58fe0533"]}]' --parameters '{"workingDirectory":[""],"executionTimeout":["3600"],"commands":["sudo su ubuntu","cd /home/ubuntu/nest-app","aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 904233112587.dkr.ecr.us-east-1.amazonaws.com","docker image prune --all --filter \"until=24h\"", "source .env", "docker compose pull && docker compose up -d"]}' --timeout-seconds 1200 --max-concurrency "50" --max-errors "0" --region us-east-1
